3e73ec98ea05d3cc06cc3b5ef3e6e49194b0c0d0
humr <humurong@hotmail.com>
2026-05-22 15:06:39 +0800

    Initial commit: Xinzhou Great Wall Museum

--- deploy.ps1
+++ deploy.ps1
@@ -1,16 +1,12 @@
 $gitExe = "D:\Program Files\Git\bin\git.exe"
 
-if (-not (Test-Path $gitExe)) {
-    Write-Host "ERROR: Git not found at $gitExe"
-    exit 1
-}
-
-Write-Host "Using Git: $gitExe"
-
 & $gitExe config --global user.name "humr"
 & $gitExe config --global user.email "humurong@hotmail.com"
+
+& $gitExe remote remove origin
 & $gitExe init
-& $gitExe add .
+& $gitExe add -A
 & $gitExe commit -m "Initial commit: Xinzhou Great Wall Museum"
+& $gitExe branch -M main
 & $gitExe remote add origin https://github.com/humurong/xinzhougw.git
-& $gitExe push -u origin main
+& $gitExe push -f -u origin main
